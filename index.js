71b37382d5e1d0a385d091eaca3e4315536a13fd
Shadin Al-Arab <shadin.alarab@gmail.com>
2022-09-12 14:53:05 -0700

    all classes added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const { off } = require("process");
+
 class Employee {
-  constructor(email, id, name){
+  constructor (email, id, name){
     this.email = email;
     this.id = id;
     this.name = name;
@@ -13,4 +15,49 @@ getID (){
 getName () {
   return this.name;
 }
+getRole () {
+return Employee;
+}
+}
+class Manager extends Employee{
+  constructor (email, id, name, officeNumber){
+    super(email);
+    super(id);
+    super(name);
+    this.officeNumber = officeNumber;
+  }
+  getOfficeNumber (){
+    return this.officeNumber;
+  }
+  getRole (){
+    return Manager;
+  }
+}
+class Engineer extends Employee{
+  constructor(email, id, name, github){
+    super(email);
+    super(id);
+    super(name);
+    this.github = github;
+  }
+  getGithub (){
+    return this.github;
+  }
+  getRole (){
+    return Engineer;
+  }
+}
+class Intern extends Employee{
+  constructor(email, id, name, school){
+    super(email);
+    super(id);
+    super(name);
+    this.school = school;
+  }
+  getSchool (){
+    return this.school;
+  }
+  getRole (){
+    return Intern;
+  }
 }
