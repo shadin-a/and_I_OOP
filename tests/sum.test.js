0c95c6361504d8f2bae2725985e26d21e2a9cf9e
Shadin Al-Arab <shadin.alarab@gmail.com>
2022-09-12 14:19:38 -0700

    first commit

---
+++ tests/sum.test.js
@@ -0,0 +1,16 @@
+class Employee {
+  constructor(email, id, name){
+    this.email = email;
+    this.id = id;
+    this.name = name;
+  }
+getEmail () {
+  return this.email;
+}
+getID (){
+  return this.id;
+}
+getName () {
+  return this.name;
+}
+}
