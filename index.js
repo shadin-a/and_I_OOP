298c6071a2c3b03ce8a855f20cb87878cb05be7f
Shadin Al-Arab <shadin.alarab@gmail.com>
2022-09-16 08:37:42 -0700

    inquirer version updated, now working

--- index.js
+++ index.js
@@ -1,63 +1,71 @@
+const { inheritInnerComments } = require('@babel/types');
+const fs = require('fs');
+const inquirer = require('inquirer');
 const { off } = require("process");
+const manager = require('/Users/shadinalarab/2022 BOOTCAMP/DEVELOP_MODULE10/module10_and_I_OOP/lib/manager.js');
+const intern = require('/Users/shadinalarab/2022 BOOTCAMP/DEVELOP_MODULE10/module10_and_I_OOP/lib/intern.js');
+const engineer = require('/Users/shadinalarab/2022 BOOTCAMP/DEVELOP_MODULE10/module10_and_I_OOP/lib/engineer.js');
 
-class Employee {
-  constructor (email, id, name){
-    this.email = email;
-    this.id = id;
-    this.name = name;
-  }
-getEmail () {
-  return this.email;
+//FUNCTION TO CREATE FILE
+function writeToFile(fileName, html) {
+  fs.writeFile(filename, html, err => {
+      if (err) {
+        console.error(err);
+      } else {
+          console.log('file written successfully');
+      }
+    });
 }
-getID (){
-  return this.id;
+//PROMPTS FOR MANAGER 
+inquirer
+  .prompt([
+  {   name: 'name',
+      message: 'What is the team managers name?',
+      type: 'input'
+
+},
+{     name: 'id',
+      message: 'What is their ID?',
+      type: 'input'
+
+},
+{     name: 'email',
+      message: 'What is their email address?',
+      type: 'input'
+
+},
+{     name: 'officeNumber',
+      message: 'What is their office number?',
+      type: 'input'
+
+},
+{
+     name: "nextStep",
+     message: "Would you like to add a Engineer or Intern to your team or are you done assembling?",
+     type: "list",
+    choices: ["Intern", "Engineer", "I'm done!"]
+   }])
+
+   //PROMPTS FOR INTERN
+
+   //PROMPTS FOR ENGINEER
+.then((answer) => {
+  console.log("Hello" + answer.name);
+  console.log("This is your ID:" + answer.id);
+  console.log("This is your email:" + answer.email);
+  console.log("This is your office number:" + answer.officeNumber);
+  console.log(answer.nextStep);
+});
+
+// function validateUserInput (){
+//   let x = userInput.value;
+//   if (x == properUserInput){
+//     alert ("Please check your input.");
+//     return false;
+//   }
+// }
+//FUNCTION TO INITIALIZE APP
+function init(){
+  writeToFile(filename, html);
 }
-getName () {
-  return this.name;
-}
-getRole () {
-return Employee;
-}
-}
-class Manager extends Employee{
-  constructor (email, id, name, officeNumber){
-    super(email);
-    super(id);
-    super(name);
-    this.officeNumber = officeNumber;
-  }
-  getOfficeNumber (){
-    return this.officeNumber;
-  }
-  getRole (){
-    return Manager;
-  }
-}
-class Engineer extends Employee{
-  constructor(email, id, name, github){
-    super(email);
-    super(id);
-    super(name);
-    this.github = github;
-  }
-  getGithub (){
-    return this.github;
-  }
-  getRole (){
-    return Engineer;
-  }
-}
-class Intern extends Employee{
-  constructor(email, id, name, school){
-    super(email);
-    super(id);
-    super(name);
-    this.school = school;
-  }
-  getSchool (){
-    return this.school;
-  }
-  getRole (){
-    return Intern;
-  }
-}
+
